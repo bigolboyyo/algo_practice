734486c055bc9549573e85dec015db64f4bb5a5a
bigolboyyo <wesley.grant94@gmail.com>
2022-11-11 14:20:19 -0500

    binary one liner, attempting string manip next

---
+++ add_binary.js
@@ -0,0 +1,29 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+//  Example 1:
+
+//  Input: a = "11", b = "1"
+//  Output: "100"
+//  Example 2:
+
+//  Input:
+let a = "1010",
+  b = "1011";
+// Output: "10101"
+
+// we are taking two binary sums as strings
+// JS converts to octal when 0 is at start of a num
+
+var addBinary = function (a, b) {
+  // one-liner :
+  // console.log((BigInt("0b" + a) + BigInt("0b" + b)).toString(2));
+  // string manipulation:
+};
+
+addBinary(a, b);
